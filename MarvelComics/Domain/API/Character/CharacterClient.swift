3d784677f945946b6d339b04220456d1c0ca4e7a
Daniel Cano Arbelaez <daniel.cano.arbelaez@gmail.com>
2022-03-03 13:00:10 -0500

    * update client functions

--- MarvelComics/Domain/API/Character/CharacterClient.swift
+++ MarvelComics/Domain/API/Character/CharacterClient.swift
@@ -10,8 +10,8 @@ import Combine
 
 class CharacterClient: APIClient, BaseCharacterClient {
     
-    func characters() -> AnyPublisher<[CharacterModel], Error> {
-        guard let url = environment.urlComponents(endpoint: APIEndpoint.characters.value).url else {
+    func characters(_ endpoint: APIEndpoint) -> AnyPublisher<[CharacterModel], Error> {
+        guard let url = environment.urlComponents(endpoint: endpoint.value).url else {
             fatalError("Cloudn' create URLComponents")
         }
         let request = URLRequest(url: url)
@@ -21,8 +21,8 @@ class CharacterClient: APIClient, BaseCharacterClient {
             .eraseToAnyPublisher()
     }
     
-    func character(by id: Int) -> AnyPublisher<CharacterModel, Error> {
-        let enpoint = APIEndpoint.characters.value + "/\(id)"
+    func character(_ endpoint: APIEndpoint, by id: Int) -> AnyPublisher<CharacterModel, Error> {
+        let enpoint = endpoint.value + "/\(id)"
         guard let url = environment.urlComponents(endpoint: enpoint).url else {
             fatalError("Cloudn' create URLComponents")
         }
